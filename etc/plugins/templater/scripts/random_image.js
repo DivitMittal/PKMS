a128205738fc517b967469cec7cffb6014906fa5
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-01-02 23:28:19 +0530

    style: apply consistent formatting to files

--- etc/plugins/templater/scripts/random_image.js
+++ etc/plugins/templater/scripts/random_image.js
@@ -2,14 +2,14 @@ let clientID = "S_iM9EBcHMEiTISRK8E6MaZP2amR6unjec4h8tRq6Y0";
 let endpoint = `https://api.unsplash.com/photos/random/?client_id=${clientID}`;
 
 const storeJsonData = fetch(endpoint, {
-  method: "GET",
+    method: "GET",
 })
-  .then((response) => response.json())
-  .catch((err) => console.log(err));
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
 
 const getData = async () => {
-  const jsonData = await storeJsonData;
-  return jsonData.urls["regular"].toString();
+    const jsonData = await storeJsonData;
+    return jsonData.urls["regular"].toString();
 };
 
 module.exports = getData;
